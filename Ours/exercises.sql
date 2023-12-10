39b1eef5243c9cf80752289518b491b78c7f5f0d
Jorge Benjumea Burillo <jorge.benjumea@alumnos.upm.es>
2023-12-10 13:03:59 +0100

    g & h

--- Ours/exercises.sql
+++ Ours/exercises.sql
@@ -1,3 +1,40 @@
+# g) Resolver en SQL la consulta: “Obtener el nombre de la revista y el total de ci-
+# tas (num citations) que hayan recibido sus artı́culos para aquella/s revista/s
+# que, perteneciendo al primer cuartil del factor de impacto (q1), tengan el mayor
+# número de citas de toda la base de datos”.
+
+SELECT j.journal_name AS journal_name, SUM(a.num_citations) AS total_citations
+FROM journal j
+JOIN article a ON j.journal_id = a.journal_id
+WHERE JIF_Quartile = 'Q1'
+GROUP BY j.journal_name
+ORDER BY total_citations DESC;
+
+# h) Resolver en SQL la consulta: “Obtener aquellas entidades (affiliation name)
+# que tienen asociados al menos 10 autores que hayan publicado más de 5 artı́culos
+# en el año más reciente que figuren en la base de datos (este año debe calcularse
+# de forma dinámica con la consulta)”.
+
+SELECT a.affiliation_name
+FROM affiliation a
+WHERE a.affiliation_id IN (
+    SELECT aa.affiliation_id
+    FROM author_affiliation aa
+    WHERE aa.author_id IN (
+        SELECT aa.author_id
+        FROM author_article aa
+        JOIN `bd-citit21-g6`.author a2 on a2.author_id = aa.author_id
+        JOIN `bd-citit21-g6`.author_affiliation aa2 on a2.author_id = aa2.author_id
+        JOIN `bd-citit21-g6`.article a3 on aa.DOI = a3.DOI
+        WHERE YEAR(a3.publication_date) = (
+            SELECT MAX(YEAR(a3.publication_date))
+            FROM article a3
+        )
+    )
+    GROUP BY aa.affiliation_id
+    HAVING COUNT(aa.author_id) >= 10
+);
+
 # m) Crear una función que, recibiendo como parámetro un identificador de una revista
 # devuelva el número medio de artı́culos por año que dicha revista ha publicado.
 # Escribir el código necesario para poder probarlo.
