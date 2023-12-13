5a78ca395754aa8b790dcad4b1f0f56f2e2a6e39
Jorge Benjumea Burillo <jorge.benjumea@alumnos.upm.es>
2023-12-13 20:30:50 +0100

    Error Handling

--- Practica_BBDD/src/main/java/Main.java
+++ Practica_BBDD/src/main/java/Main.java
@@ -8,13 +8,15 @@ public class Main {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         } catch (Exception ex) {
             System.out.println("Whoops, exception error!");
+            ex.printStackTrace();
             System.exit(1);
         }
         Connection conn = null;
         try{
             conn = DriverManager.getConnection("jdbc:mysql://contabo:6033", "bd-citit21-g6", "le_cwoissant69");
         } catch (Exception ex) {
-            System.out.println("Uh oh");
+            System.out.println("Uh oh, something that I don't know happened!");
+            ex.printStackTrace();
             System.exit(2);
         }
     }
