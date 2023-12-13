609e67da0b7f11c8c0a4001a5901d6bca7da9182
Jorge Benjumea Burillo <jorge.benjumea@alumnos.upm.es>
2023-12-13 20:30:37 +0100

    New imports

--- Practica_BBDD/src/main/java/Main.java
+++ Practica_BBDD/src/main/java/Main.java
@@ -1,6 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
