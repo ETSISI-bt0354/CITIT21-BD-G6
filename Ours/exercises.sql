a5e84709ee0dd9f49d7accccbbc5fb08651838ef
Jorge Benjumea Burillo <jorge.benjumea@alumnos.upm.es>
2023-12-21 16:08:00 +0100

    Removed unnecesary JOIN for real

--- Ours/exercises.sql
+++ Ours/exercises.sql
@@ -48,7 +48,6 @@ JOIN affiliation ON author_affiliation.affiliation_id = affiliation.affiliation_
 WHERE affiliation.country_name = 'spain'
 AND author.author_id NOT IN (SELECT author_article.author_id
     FROM author_article
-	-- JOIN author_article ON author.author_id = author_article.author_id
 	JOIN article ON author_article.DOI = article.DOI
 	WHERE YEAR(article.publication_date) = 2021 OR YEAR(article.publication_date) = 2020)
 ORDER BY affiliation_name, author.author_name ASC;
